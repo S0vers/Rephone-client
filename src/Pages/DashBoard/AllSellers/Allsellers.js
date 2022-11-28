2e96cb8c30bf08dfc0dda522df8107e2135a490f
S0vers <sovers.pandey@outlook.com>
2022-11-28 20:29:04 +0600

    user based routes set and all sellers added

--- src/Pages/DashBoard/AllSellers/Allsellers.js
+++ src/Pages/DashBoard/AllSellers/Allsellers.js
@@ -3,11 +3,12 @@ import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../Context/AuthProvider';
 import Loading from '../../Shared/Loading/Loading';
-
+import { MdDeleteForever } from 'react-icons/md';
+import { MdVerified } from "react-icons/md";
 const Allsellers = () => {
     const { logout } = useContext(AuthContext)
     const url = 'http://localhost:5000/users/seller'
-    const { data: seller, isLoading, refetch } = useQuery({
+    const { data: sellers, isLoading, refetch } = useQuery({
         queryKey: ['sellers'],
         queryFn: async () => {
             const res = await fetch(url, {
@@ -17,18 +18,44 @@ const Allsellers = () => {
             })
             const data = await res.json();
             if (res.status === 401 || res.status === 403) {
-                console.log('HIT')
+                return logout();
             }
             return data;
         }
     })
-    console.log(seller)
+    console.log(sellers)
     if (isLoading) {
         return <Loading></Loading>
     }
     return (
-        <div>
-            This is all sellers.
+        <div className="overflow-x-auto">
+            <h3 className="text-4xl font-bold text-center text-primary my-10">List of all the Sellers</h3>
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Verification</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        sellers.map((seller, i) => <tr key={seller._id}>
+                            <th>{i + 1}</th>
+                            <td>{seller.name}</td>
+                            <td>{seller.email}</td>
+                            <td>{seller?.isVerified ?
+                                <p className='text-3xl text-blue-400'><MdVerified></MdVerified></p>
+                                :
+                                <button className='text-white btn btn-xs bg-green-400 hover:bg-blue-400 border-none'>Verify Seller</button>
+                            }</td>
+                            <td><label htmlFor="confirmation-modal" className='hover:text-red-600 text-3xl text-center'><MdDeleteForever></MdDeleteForever></label></td>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -5,7 +5,7 @@ const DashBoard = () => {
     const { user } = useContext(AuthContext)
     return (
         <div className='my-10'>
-            <h3 className="text-4xl font-bold text-center text-primary">Welcome to dashboard {user.displayName}</h3>
+            <h3 className="text-4xl font-bold text-center text-primary">Welcome To Dashboard {user.displayName}</h3>
         </div>
     );
 };
