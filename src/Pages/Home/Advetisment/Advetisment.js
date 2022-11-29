7953a25f909e097673b0115f24bd545ceab34241
S0vers <sovers.pandey@outlook.com>
2022-11-29 14:16:38 +0600

    advetisment section added

--- src/Pages/Home/Advetisment/Advetisment.js
+++ src/Pages/Home/Advetisment/Advetisment.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import AdvetismentCard from "./AdvetismentCard";
 
-const Advetisment = () => {
+const Advetisment = ({ advetisments }) => {
+    console.log(advetisments)
     return (
-        <div className='mt-10'>
+        <div className='my-10'>
             <h3 className="text-4xl font-bold text-center text-primary">Advertisement</h3>
             <div className='mt-32 grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    advetisments.map(ads => <AdvetismentCard
+                        key={ads._id}
+                        Advetisment={ads}
+                    >
 
+                    </AdvetismentCard>)
+                }
             </div>
         </div>
     );
--- src/Pages/Home/Advetisment/AdvetismentCard.js
+++ src/Pages/Home/Advetisment/AdvetismentCard.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const AdvetismentCard = () => {
+const AdvetismentCard = ({ Advetisment }) => {
+    const { ProductImg, name, resalePrice } = Advetisment
     return (
         <div className="card mx-auto w-full lg:w-96 bg-blue-100 shadow-xl">
             <figure className="px-10 pt-10">
-                {/* <img src={brandIcon} alt="Shoes" className="rounded-xl w-32" /> */}
+                <img src={ProductImg} alt="Shoes" className="rounded-xl w-64" />
             </figure>
             <div className="card-body items-center text-center">
-                <h2 className="card-title text-3xl">feef</h2>
-                <p>deaasje</p>
+                <h2 className="card-title text-3xl">{name}</h2>
+                <p className='text-green-600'>Price: ${resalePrice}</p>
             </div>
         </div>
     );
